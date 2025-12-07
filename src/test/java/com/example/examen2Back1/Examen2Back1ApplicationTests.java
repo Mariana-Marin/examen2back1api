d00ca2b3165e402195f0e5216d5a4e6e7d410c17
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-12-07 11:34:39 -0500

    muchos exitos

---
+++ src/test/java/com/example/examen2Back1/Examen2Back1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examen2Back1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2Back1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
